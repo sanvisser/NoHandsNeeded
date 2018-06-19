4f9e7a89fe3e37387526e3350e5aeb4edf17b424
sander.visser@xillio.nl <sanvisser@gmail.com>
2018-06-19 07:58:21 +0200

    Add tooltip

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 let commands = {
-    'show me :color on :area': commandColorArea
+    'show me :color on :area': commandColorArea,
+    'color :number :color': commandColorNumber,
+    'toggle': ToggleToolTips
 };
+
+let tooltips = {};
 
 function init() {
 
@@ -17,19 +21,19 @@ function init() {
     registerAnnyangCommands();
 }
 
-function registerAnnyangCommands(){
-    if(annyang) {
+function registerAnnyangCommands() {
+    if (annyang) {
         // Add the commands to annyang
         annyang.addCommands(commands);
 
         // Start listening.
         annyang.start();
-    }else {
+    } else {
         console.error("annyang failed to start.");
     }
 }
 
-function commandColorArea(color, area){
+function commandColorArea(color, area) {
     switch (area) {
         case "left":
             document.getElementById('div1').style.backgroundColor = color;
@@ -42,4 +46,32 @@ function commandColorArea(color, area){
             break;
     }
 }
+
+function commandColorNumber(number, color){
+    console.log(number, color);
+    if(number.isNumeric()) {
+        let tooltip = tooltips[number];
+        tooltip.reference.style.backgroundColor = color;
+    }
+    console.log("no numeric number found");
+}
+
+function ToggleToolTips(){
+    for (let key in tooltips) {
+        if(tooltips.hasOwnProperty(key)){
+            tooltips[key].toggle();
+        }
+    }
+}
+
+window.addEventListener('load', function () {
+    let triggers = document.getElementsByClassName("block");
+    for (let i = 0; i < triggers.length; i++) {
+        tooltips[i] = (new Tooltip(triggers[i], {
+            title: "( " + i + " )",
+            trigger: 'manual'
+        }));
+    }
+});
+
 
